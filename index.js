169fcb9f75d5385ec36b0725deae5637e6f39e7c
lrbn86 <lrbn86@gmail.com>
2022-03-02 14:40:29 -0500

    Merged all the eventlisteners into one function called EventListeners(). Now working on creating text and comments

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
 svg.setAttribute('id', 'svg')
 
 const nodeRadius = 50;
-const svgSize = 1000;
 let numNodes = 5;
-let zoomLevel = 1250;
 
 const zoomSlider = document.querySelector('#zoom-slider');
+const zoomLevelLabel = document.querySelector('#zoom-level');
+let zoomLevel = 1250;
 
 // Everything begins here.
 function startApp() {
@@ -21,9 +21,7 @@ function initialize() {
   zoomSlider.value = zoomLevel;
   svg.setAttribute('viewBox', `0 0 ${zoomLevel} ${zoomLevel}`);
   main.appendChild(svg);
-  toggleMousePanningZooming();
-  toggleDrag();
-  toggleDrawNode();
+  EventListeners();
   let inc = 350;
   for (let i = 0; i < numNodes; i++) {
     drawNode(inc, 350);
@@ -45,65 +43,94 @@ function drawNode(x, y) {
   svg.appendChild(node);
 }
 
-// TODO: The toggleMousePanningZooming() and toggleDrag() both share the same eventlisteners.
-//       we may consider combining them...or just seperate..? 
-// This function controls the panning and zooming functionalities.
-// The zooming is always active.
-let toggleMousePanningZoomingFlag = false;
-function toggleMousePanningZooming() {
-  // source: https://css-tricks.com/creating-a-panning-effect-for-svg/
-  let drag = false;
+const svgPoint = svg.createSVGPoint();
+const svgSize = 1000;
+let togglePanningFlag = false;
+let toggleDragObjectFlag = true;
+let toggleDrawNodeFlag = false;
+let toggleDrawTextFlag = false;
+
+function EventListeners() {
+  
+  let isDragging = false;
+  let selectedObject = null;
+  
   let pointerOrigin = {
     x: 0,
     y: 0
   };
+  
   let currentViewBox = {
     x: 0,
     y: 0,
     width: zoomLevel,
     height: zoomLevel
   };
+  
   let newViewBox = {
     x: 0,
     y: 0
   };
+
   svg.addEventListener('mousedown', (event) => {
-    drag = true;
-    // Set the current mouse position in pointerOrigin
+    // Handle panning
+    isDragging = true;
     pointerOrigin.x = event.x;
     pointerOrigin.y = event.y;
-  });
-  svg.addEventListener('mouseup', (event) => {
-    drag = false;
-    // If the user is no longer holding the mouse down,
-    // set the current view to the new view
-    currentViewBox.x = newViewBox.x;
-    currentViewBox.y = newViewBox.y;
-  });
-  svg.addEventListener('mouseleave', (event) => {
-    // The user can no longer pan if the mouse is not in the SVG view
-    drag = false;
-  });
-  svg.addEventListener('mousemove', (event) => {
-    if (drag && toggleMousePanningZoomingFlag) {
-      // If we are in panning/dragging mode, then calculate the new view position
-      // the zoom level will remain consistent with the zoom slider
-      newViewBox.x = currentViewBox.x - (event.x - pointerOrigin.x);
-      newViewBox.y = currentViewBox.y - (event.y - pointerOrigin.y);
-      console.log(newViewBox.x, newViewBox.y)
-      svg.setAttribute('viewBox', `${newViewBox.x} ${newViewBox.y} ${zoomLevel} ${zoomLevel}`);
+    
+    // Handle object dragging
+    // Objects will be identified by their class name
+    if (event.target.getAttribute('class') === 'node') {
+      selectedObject = event.target;
+    }
+
+    // Handle node creation
+    if (toggleDrawNodeFlag) {
+      // Convert screen coordinates to SVG coordinate
+      svgPoint.x = event.x;
+      svgPoint.y = event.y;
+      const matrix = svgPoint.matrixTransform(svg.getScreenCTM().inverse());
+      drawNode(matrix.x, matrix.y);
+    }
+
+    if (toggleDrawTextFlag) {
     }
   });
-  // Control zoom
-  const zoomLevelLabel = document.querySelector('#zoom-level');
-  zoomSlider.addEventListener('input', (event) => {
-    const value = event.target.value;
-    zoomLevel = value;
-    // TODO: Calculate zoom percentage
-    zoomLevelLabel.textContent = zoomLevel + '%';
-    svg.setAttribute('viewBox', `${currentViewBox.x} ${currentViewBox.y} ${zoomLevel} ${zoomLevel}`);
+  
+  svg.addEventListener('mouseup', (event) => {
+    isDragging = false;
+    currentViewBox.x = newViewBox.x;
+    currentViewBox.y = newViewBox.y;
+
+    selectedObject = null;
   });
-  // Detect mouse wheel for zooming
+  
+  svg.addEventListener('mouseleave', (event) => {
+    isDragging = false;
+  });
+
+
+  svg.addEventListener('mousemove', (event) => {
+    // Handle panning
+    if (isDragging && togglePanningFlag) {
+      newViewBox.x = currentViewBox.x - (event.x - pointerOrigin.x);
+      newViewBox.y = currentViewBox.y - (event.y - pointerOrigin.y);
+      svg.setAttribute('viewBox', `${newViewBox.x} ${newViewBox.y} ${zoomLevel} ${zoomLevel}`)
+    }
+    
+    // Handle object dragging
+    if (selectedObject && toggleDragObjectFlag) {
+      // Convert screen coordinates to SVG coordinate
+      svgPoint.x = event.x;
+      svgPoint.y = event.y;
+      const matrix = svgPoint.matrixTransform(svg.getScreenCTM().inverse());
+      selectedObject.setAttribute('cx', matrix.x);
+      selectedObject.setAttribute('cy', matrix.y);
+    }
+
+  });
+  
+  // Handle zooming with the mouse
   svg.addEventListener('wheel', (event) => {
     const deltaY = event.deltaY;
     const inc = 10;
@@ -112,61 +139,26 @@ function toggleMousePanningZooming() {
     } else if (deltaY > 0) { // Zooming out
       zoomSlider.value = parseInt(zoomSlider.value) + inc; 
     }
-
     // This dispatch is required so that if the user uses the wheel on the mouse, it will trigger the slider
     // to change accordingly. Without this, the slider will not be in the appropriate position.
     zoomSlider.dispatchEvent(new Event('input'));
   });
+
+  // Handle zooming on the UI
+  zoomSlider.addEventListener('input', (event) => {
+    const value = event.target.value;
+    zoomLevel = value;
+    zoomLevelLabel.textContent = zoomLevel + '%';
+    svg.setAttribute('viewBox', `${currentViewBox.x} ${currentViewBox.y} ${zoomLevel} ${zoomLevel}`)
+  })
 }
 
-// This function controls the dragging functionality.
-let toggleDragFlag = true;
-function toggleDrag() {
-  let selected = null;
-  function dragStart(event) {
-    let target = event.target;
-    if (target.getAttribute('class') === 'node') {
-      selected = target;
-    }
-  }
-  function dragEnd(event) {
-    selected = null; // Deselect object
-  }
-  const svgPoint = svg.createSVGPoint();
-  function drag(event) {
-    if (selected && toggleDragFlag) {
-      // Convert screen coordinates to SVG coordinates
-      const x = event.x; // The screen mouse X position
-      const y = event.y; // The screen mouse Y position
-      // Store the screen mouse's x and y and convert them using matrixTransform
-      svgPoint.x = x;
-      svgPoint.y = y;
-      const matrix = svgPoint.matrixTransform(svg.getScreenCTM().inverse());
-      const cx = matrix.x; // The SVG-coordinate mouse X position
-      const cy = matrix.y; // The SVG-coordinate mouse Y position
-      selected.setAttribute('cx', cx);
-      selected.setAttribute('cy', cy);
-    }
-  }
-  // The SVG object will be listening to the following mouse events
-  svg.addEventListener('mousedown', dragStart);
-  svg.addEventListener('mouseup', dragEnd);
-  svg.addEventListener('mousemove', drag);
-}
 
-// This function controls the create node functionality
-let toggleDrawNodeFlag = false;
-function toggleDrawNode() {
-  const svgPoint = svg.createSVGPoint();
-  svg.addEventListener('mousedown', (event) => {
-    if (toggleDrawNodeFlag) {
-      // Convert screen coordinates to SVG coordinate
-      svgPoint.x = event.x;
-      svgPoint.y = event.y;
-      const matrix = svgPoint.matrixTransform(svg.getScreenCTM().inverse());
-      drawNode(matrix.x, matrix.y);
-    }
-  });
+function offFlag() {
+  togglePanningFlag = false;
+  toggleDragObjectFlag = false;
+  toggleDrawNodeFlag = false;
+  toggleDrawTextFlag = false;
 }
 
 // This function handles all buttons interactivity on the UI.
@@ -186,34 +178,26 @@ function buttonEvents() {
       // Determine which button was actually clicked and toggle the appropriate functionality
       switch (btn.id) {
         case 'pointer-btn':
+          offFlag();
           toggleDragFlag = true;
-          toggleMousePanningZoomingFlag = false;
-          toggleDrawNodeFlag = false;
           break;
         case 'pan-btn':
-          toggleDragFlag = false;
-          toggleMousePanningZoomingFlag = true;
-          toggleDrawNodeFlag = false;
+          offFlag();
+          togglePanningFlag = true;
           break;
         case 'create-node-btn':
-          toggleDragFlag = false;
-          toggleMousePanningZoomingFlag = false;
+          offFlag();
           toggleDrawNodeFlag = true;
           break;
         case 'create-line-btn':
-          toggleDragFlag = false;
-          toggleMousePanningZoomingFlag = false;
-          toggleDrawNodeFlag = false;
+          offFlag();
           break;
         case 'create-text-btn':
-          toggleDragFlag = false;
-          toggleMousePanningZoomingFlag = false;
-          toggleDrawNodeFlag = false;
+          offFlag();
+          toggleDrawTextFlag = true;
           break;
         case 'create-comment-btn':
-          toggleNodeDragFlag = false;
-          toggleMousePanningZoomingFlag = false;
-          toggleDrawNodeFlag = false;
+          offFlag();
           break;
       }
     });
