7e9ff4333547b47e5e08d7bc0da6752fc16fb0d7
lrbn86 <lrbn86@gmail.com>
2022-03-28 22:58:12 -0400

    Update

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const lineStrokeWidth = 20;
 let currentNumNodes = 0;
 let currentNumEdges = 0;
 
+const NS = 'http://www.w3.org/2000/svg'; 
+
+
 const zoomSlider = document.querySelector('#zoom-slider');
 const zoomLevelLabel = document.querySelector('#zoom-level');
 // Get attributes from the zoomSlider element
@@ -17,7 +20,6 @@ let minRange = parseInt(zoomSlider.getAttribute('min'));
 let stepRange = parseInt(zoomSlider.getAttribute('step'));
 let zoomLevel = 1250; // Get the middle number
 let zoomPercentage = 100; // Default percentage is 100%
-
 
 function main() {
   initialize();
@@ -33,31 +35,27 @@ function initialize() {
   UIEventListeners();
 }
 
-
 // These containers help control what should be drawn first since SVG is based on the painter-model
 // e.g. the nodes will be drawn on top of the edges
-const edgesContainer = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+const edgesContainer = document.createElementNS(NS, 'g');
 edgesContainer.setAttribute('id', 'edges-container');
 svg.appendChild(edgesContainer);
 
-const nodesContainer = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+const nodesContainer = document.createElementNS(NS, 'g');
 nodesContainer.setAttribute('id', 'nodes-container');
 svg.appendChild(nodesContainer);
 
-const commentContainer = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+const commentContainer = document.createElementNS(NS, 'g');
 commentContainer.setAttribute('id', 'comment-container');
 svg.appendChild(commentContainer);
 
-const textContainer = document.createElementNS('http://www.w3.org/2000/svg', 'g');
-textContainer.setAttribute('id', 'text-container');
-svg.appendChild(textContainer);
+const tasksContainer = document.createElementNS(NS, 'g');
+tasksContainer.setAttribute('id', 'tasks-container');
+svg.appendChild(tasksContainer);
 
-drawTaskBox(450, 450);
+drawTaskBox(350, 350);
 
 let graph = {};
-let connectedNodes = [];
-let nodesEdges = {};
-let selectedNodes = [];
 
 const svgPoint = svg.createSVGPoint();
 
@@ -68,9 +66,8 @@ function SVGEventListeners() {
   let pointerOrigin = { x: 0, y: 0 };
   let currentViewBox = { x: 0, y: 0, width: zoomLevel, height: zoomLevel };
   let newViewBox = { x: 0, y: 0 };
-
   function convertToSVGCoordinates(x, y) { svgPoint.x = x; svgPoint.y = y; return svgPoint.matrixTransform(svg.getScreenCTM().inverse()); }
-  
+
   svg.addEventListener('mousedown', (event) => {
 
     const target = event.target;
@@ -78,76 +75,28 @@ function SVGEventListeners() {
     const targetClass = target.getAttribute('class');
     const matrix = convertToSVGCoordinates(event.x, event.y);
     
-    // Right click to change color of nodes/edges
-    if (event.button === 2) {
-      if (targetClass === 'node') {
-        const color = target.getAttribute('fill') === normalColor ? criticalColor : normalColor;
-        target.setAttribute('fill', color);
-      } else if (targetClass === 'line') {
-        const color = target.getAttribute('stroke') === normalColor ? criticalColor : normalColor;
-        target.setAttribute('stroke', color);
-      }
-      return;
-    }
-
     if (UIMode['select-btn'][0]) {
-
       if (targetClass === 'node') {
-
         selectedObject = target;
-        selectedObject.setAttribute('stroke', normalColor);
-        selectedObject.setAttribute('stroke-width', '45');
-        selectedObject.setAttribute('stroke-opacity', '.2');
-
       }
-
     }
 
     if (UIMode['move-btn'][0]) {
-
       isPanning = true;
       pointerOrigin.x = event.x;
       pointerOrigin.y = event.y;
-
     }
 
     // Handle node creation
     if (UIMode['add-node-btn'][0]) {
-      // Make sure that we are not holding an object before we draw a node
-      if (!selectedObject) {
-        // Don't draw on top of another node or edge
-        if (targetClass !== 'node' && targetClass !== 'line') {
-          drawNode(matrix.x, matrix.y);
-          graph[`node${currentNumNodes}`] = [];
-          nodesEdges[`node${currentNumNodes}`] = [];
-        }
-      }
     }
 
     if (UIMode['connect-nodes-btn'][0]) {
-
-      if (targetClass === 'node') {
-        target.setAttribute('stroke', normalColor);
-        target.setAttribute('stroke-width', '45');
-        target.setAttribute('stroke-opacity', '.2');
-        selectedNodes.push(target.getAttribute('id'));
-        if (selectedNodes.length > 1) {
-          const [nodeA, nodeB] = selectedNodes;
-          if (nodeA !== nodeB) connectNodes(nodeA, nodeB);
-        }
-      }
-
-      if (targetID === 'svg') {
-        selectedNodes.forEach((node) => { const nodeDOM = document.getElementById(node); nodeDOM.setAttribute('stroke', 'none') });
-        selectedNodes = [];
-      }
-
     }
 
     // TODO:
     if (UIMode['add-task-btn'][0]) {
-      console.log(target.parentNode);
-        // drawTaskBox(matrix.x, matrix.y);
+      drawTaskBox(matrix.x, matrix.y);
     }
 
     // TODO:
@@ -156,18 +105,10 @@ function SVGEventListeners() {
     }
 
   });
-  
+
   svg.addEventListener('mouseup', (event) => {
 
     if (UIMode['select-btn'][0]) {
-
-      if (selectedObject && selectedObject.getAttribute('class') === 'node') {
-
-        selectedObject.setAttribute('stroke', 'none');
-
-      }
-
-      selectedObject = null;
 
     }
 
@@ -182,36 +123,23 @@ function SVGEventListeners() {
   });
   
   svg.addEventListener('mousemove', (event) => {
+
     const matrix = convertToSVGCoordinates(event.x, event.y);
 
     if (UIMode['move-btn'][0]) {
+
       if (isPanning) {
+
         newViewBox.x = currentViewBox.x - (event.x - pointerOrigin.x);
         newViewBox.y = currentViewBox.y - (event.y - pointerOrigin.y);
         svg.setAttribute('viewBox', `${newViewBox.x} ${newViewBox.y} ${zoomLevel} ${zoomLevel}`);
+
       }
+
     }
-    
+
     if (UIMode['select-btn'][0]) {
 
-      if (selectedObject) {
-
-        selectedObject.setAttribute('cx', matrix.x);
-        selectedObject.setAttribute('cy', matrix.y);
-        
-        // Update the positions of the nodes and edges
-        if (connectedNodes) {
-          connectedNodes.forEach((group) => {
-            const nodeA = group[0];
-            const nodeB = group[1];
-            const edge = group[2];
-            edge.setAttribute('x1', nodeA.getAttribute('cx'));
-            edge.setAttribute('y1', nodeA.getAttribute('cy'));
-            edge.setAttribute('x2', nodeB.getAttribute('cx'));
-            edge.setAttribute('y2', nodeB.getAttribute('cy'));
-          });
-        }
-      }
     }
 
   });
@@ -274,69 +202,6 @@ function drawComment(x, y) {
   return object;
 }
 
-// Draw a node at a point
-function drawNode(x, y) {
-  const node = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-  currentNumNodes++;
-  node.setAttribute('id', `node${currentNumNodes}`);
-  node.setAttribute('class', 'node');
-  node.setAttribute('r', nodeRadius);
-  node.setAttribute('fill', normalColor);
-  node.setAttribute('cx', x);
-  node.setAttribute('cy', y);
-  nodesContainer.appendChild(node);
-  return node;
-}
-
-// Delete a specific node
-function deleteNode(targetID) {
-  // Remove the deleted node references in graph
-  for (const id in graph) {
-    const arr = graph[id];
-    const index = arr.indexOf(targetID);
-    if (index > -1) arr.splice(index, 1);
-  }
-  // Remove the node from graph
-  delete graph[targetID];
-  // Remove the node from the DOM
-  nodesContainer.removeChild(document.getElementById(targetID));
-
-  // Delete all the edges that were connected to that deleted node
-  const edgesToDelete = nodesEdges[targetID];
-  for (const id in nodesEdges) nodesEdges[id] = nodesEdges[id].filter((edgeID) => !edgesToDelete.includes(edgeID));
-  // Remove the reference to the deleted node in nodesEdges
-  delete nodesEdges[targetID];
-  // Remove the edges from the DOM
-  edgesToDelete.forEach((edgeID) => edgesContainer.removeChild(document.getElementById(edgeID)));
-  
-  selectedNodes = [];
-  selectedObject = null;
-}
-
-// Connect two nodes with an edge/line
-function connectNodes(nodeA, nodeB) {
-  if (!graph[nodeA].includes(nodeB) && !graph[nodeB].includes(nodeA)) {
-    graph[nodeA].push(nodeB);
-    graph[nodeB].push(nodeA);
-    const x1 = document.getElementById(nodeA).getAttribute('cx');
-    const y1 = document.getElementById(nodeA).getAttribute('cy');
-    const x2 = document.getElementById(nodeB).getAttribute('cx');
-    const y2 = document.getElementById(nodeB).getAttribute('cy');
-    const edge = drawEdge(x1, y1, x2, y2);
-    nodesEdges[nodeA].push(edge.getAttribute('id'));
-    nodesEdges[nodeB].push(edge.getAttribute('id'));
-    connectedNodes.push([document.getElementById(nodeA), document.getElementById(nodeB), edge]);
-    setTimeout(() => {
-      document.getElementById(nodeA).setAttribute('stroke', 'none');
-      document.getElementById(nodeB).setAttribute('stroke', 'none');
-    }, 500);
-  } else {
-    selectedNodes.forEach((nodeID) => document.getElementById(nodeID).setAttribute('stroke', 'none'));
-  }
-  selectedObject = null;
-  selectedNodes = [];
-}
-
 // Create an edge/line between two points
 function drawEdge(x1, y1, x2, y2) {
   const edge = document.createElementNS('http://www.w3.org/2000/svg', 'line');
@@ -359,17 +224,23 @@ function drawTaskBox(x, y) {
   taskBox.setAttribute('x', x);
   taskBox.setAttribute('y', y);
   taskBox.setAttribute('width', '335');
-  taskBox.setAttribute('height', '102');
+  taskBox.setAttribute('height', '130');
   const div = document.createElement('div');
   div.setAttribute('class', 'taskbox');
   // div.setAttribute('xmlns', "http://www.w3.org/1999/xhtml");
   div.innerHTML = `
-    <p contenteditable>Task</p>
-    <p contenteditable>Assigned member</p>
-    <p contenteditable>Estimated Time</p>
+      <p>Task: <span contenteditable>_</span></p>
+      <p>Assigned: <span contenteditable>_</span></p>
+      <p contenteditable>Estimated Time</p>
+      <input type="date" />
+    <select name='status'>
+      <option value='Completed'>Completed</option>
+      <option value='In Progress'>In Progress</option>
+      <option value='Delayed'>Delayed</option>
+    </select>
   `;
   taskBox.appendChild(div);
-  textContainer.appendChild(taskBox);
+  tasksContainer.appendChild(taskBox);
 }
 
 // Determine what mode we are currently in
@@ -432,16 +303,3 @@ function UIEventListeners() {
   }
 
 }
-
-
-
-/**
- * TODO:
- * 1. Draw the nodes
- * 2. Connect the nodes with edge, the user will continue to draw polyline/line (little nodes, might be complicated) until another node is selected or user presses esc in connect nodes mode.
- * 3. Create task boxes on top of the edges manually, how can we relate the connectivity between task boxes?
- * 4. The nodes, edges, and boxes cannot be moved in selection mode to keep things simple, or they can be moved 
- *    Nodes, edges, and boxes can only be deleted and recreated to "move" them to a new place
- * 5. Once the user select save changes, grab all the information from the node dates and task boxes and relate them into a json/object
- * 
- */
