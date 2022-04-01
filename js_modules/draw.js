eadf14bdb6b231a410cb7259bbf236cdda408536
lrbn86 <lrbn86@gmail.com>
2022-04-01 04:17:51 -0400

    update

--- js_modules/draw.js
+++ js_modules/draw.js
@@ -9,48 +9,56 @@ const textContainer = doc.querySelector('#text-container');
 
 export function drawTask(x, y) {
 
-  const g = document.createElementNS(NS, 'g');
-  
-  const node = document.createElementNS(NS, 'circle');
-  node.setAttribute('fill', normalColor);
-  node.setAttribute('cx', y );
-  node.setAttribute('cy', x);
-  node.setAttribute('r', '45');
-  
-  const taskBox = document.createElementNS(NS, 'foreignObject');
   const width = 340; 
   const height = 120;
+
+  const centerX = x + (width / 2);
+  const centerY = y + (height / 2);
+
+  const g = document.createElementNS(NS, 'g');
+  
+  const taskBox = document.createElementNS(NS, 'foreignObject');
   taskBox.setAttribute('class', 'taskbox');
   taskBox.setAttribute('width', width);
   taskBox.setAttribute('height', height);
   taskBox.setAttribute('x', x);
   taskBox.setAttribute('y', y);
+  taskBox.setAttribute('data-centerX', centerX);
+  taskBox.setAttribute('data-centerY', centerY);
+
   const div = document.createElement('div');
   div.innerHTML = `
-  <input class='task-name' type='text' placeholder='Enter task name / description' />
-  <input class='team-name' type='text' placeholder='Enter team name' />
-  <input class='assignee-name' type='text' placeholder='Enter assignee name' />
-  <input class='expected-time' type='text' placeholder='Enter expected time' />
-  <label>Status:</label>
-  <select>
-  <option>-</option>
-  <option>Completed</option>
-  <option>In Progress</option>
-  <option>Delayed</option>
-  </select>
+    <input class='task-name' type='text' placeholder='Enter task name / description' />
+    <input class='team-name' type='text' placeholder='Enter team name' />
+    <input class='assignee-name' type='text' placeholder='Enter assignee name' />
+    <input class='expected-time' type='text' placeholder='Enter expected time' />
+    <label>Status:</label>
+    <select>
+      <option>-</option>
+      <option>Completed</option>
+      <option>In Progress</option>
+      <option>Delayed</option>
+    </select>
   `;
+
   div.setAttribute('class', 'taskboxDIV');
   taskBox.appendChild(div);
   g.appendChild(taskBox);
-  g.appendChild(node);
   nodeContainer.appendChild(g);
   
 }
 
-export function drawLine(nodeA, nodeB) {
+export function drawLine(x1, y1, x2, y2) {
   
-  const line = createElementNS(NS, 'line');
-  lineContainer.appendChild(lineContainer);
+  const line = document.createElementNS(NS, 'line');
+  line.setAttribute('class', 'line');
+  line.setAttribute('stroke', normalColor);
+  line.setAttribute('stroke-width', '10');
+  line.setAttribute('x1', x1);
+  line.setAttribute('y1', y1);
+  line.setAttribute('x2', x2);
+  line.setAttribute('y2', y2);
+  lineContainer.appendChild(line);
 
 }
 
