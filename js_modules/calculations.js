5503e7caeecf1cdbd29a1afb51aa6affd899aef1
lrbn86 <lrbn86@gmail.com>
2022-04-02 02:46:17 -0400

    update

--- js_modules/calculations.js
+++ js_modules/calculations.js
@@ -1,4 +1,4 @@
 export function calculateExpectedTime(tp, to, tl) {
   const result = (Number(tp) + (4 * Number(tl)) + Number(to)) / 6;
-  return Math.round(result * 100) / 100;
+  return Math.round(result * 100) / 100; // Round two decimal places
 }
